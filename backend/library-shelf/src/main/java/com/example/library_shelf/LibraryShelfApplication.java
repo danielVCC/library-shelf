2e89ac1fb9a96faa058b64cc17eec34c59e3453c
Daniel Cavalcante <dvictor.cintra@gmail.com>
2025-01-01 23:07:48 -0300

    init: project setup with Angular and Spring Boot

---
+++ backend/library-shelf/src/main/java/com/example/library_shelf/LibraryShelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryShelfApplication.class, args);
+	}
+
+}
