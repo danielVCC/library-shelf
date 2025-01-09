71492a2c0279582f0bcba1a261fdfbee2d96e741
Daniel Cavalcante <dvictor.cintra@gmail.com>
2025-01-09 00:03:38 -0300

    feat: fetch book details in BookDetailComponent using route parameters

--- frontend/src/app/pages/book-detail/book-detail.component.ts
+++ frontend/src/app/pages/book-detail/book-detail.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../../services/book/book.service';
 
 @Component({
   selector: 'app-book-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-detail.component.html',
-  styleUrl: './book-detail.component.css'
+  styleUrl: './book-detail.component.css',
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit {
+  bookId!: number;
+  bookDetails: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    // get book id from params
+    this.bookId = Number(this.route.snapshot.paramMap.get('id'));
+
+    // get book details by ID
+    this.bookService.getBookById(this.bookId).subscribe(
+      (data) => {
+        this.bookDetails = data;
+      },
+      (error) => {
+        console.error('Error fetching book data:', error);
+      }
+    );
+  }
 }
