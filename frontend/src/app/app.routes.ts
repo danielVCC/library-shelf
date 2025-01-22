1cf8c099d2d632f74aaa92e0ef377f6f5d94a1ca
Daniel Cavalcante <dvictor.cintra@gmail.com>
2025-01-22 13:57:35 -0300

    feat(frontend): add routes for authors

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { BooksComponent } from './pages/books/books.component';
 import { BookDetailComponent } from './pages/book-detail/book-detail.component';
 import { AddBookComponent } from './pages/add-book/add-book.component';
+import { AuthorsComponent } from './pages/authors/authors.component';
+import { AuthorDetailComponent } from './pages/author-detail/author-detail.component';
+import { AddAuthorComponent } from './pages/add-author/add-author.component';
 // import { AboutComponent } from './pages/about/about.component';
 // import { ContactComponent } from './pages/contact/contact.component';
 
@@ -11,6 +14,9 @@ export const routes: Routes = [
   { path: 'books', component: BooksComponent }, // Book catalog
   { path: 'books/:id', component: BookDetailComponent },
   { path: 'add-book', component: AddBookComponent },
+  { path: 'authors', component: AuthorsComponent },
+  { path: 'authors/:id', component: AuthorDetailComponent },
+  { path: 'add-author', component: AddAuthorComponent },
   //   { path: 'about', component: AboutComponent },
   //   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }, // If route does not exist, redirect to home
